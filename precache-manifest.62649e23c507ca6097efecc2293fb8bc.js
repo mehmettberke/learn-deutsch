5d5ad5cb034cb2ec732a397b3c45d05eee77acd7
mehmettberke <dxd3d9@gmail.com>
2022-10-21 22:33:28 +0300

    Updates

---
+++ precache-manifest.62649e23c507ca6097efecc2293fb8bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cffb51541aa209423e6",
+    "url": "/deutsch/static/css/main.d11e9282.chunk.css"
+  },
+  {
+    "revision": "7cffb51541aa209423e6",
+    "url": "/deutsch/static/js/main.7cffb515.chunk.js"
+  },
+  {
+    "revision": "893581d9c183f7f60d4b",
+    "url": "/deutsch/static/js/1.893581d9.chunk.js"
+  },
+  {
+    "revision": "6597c249775e02916e78",
+    "url": "/deutsch/static/js/runtime~main.6597c249.js"
+  },
+  {
+    "revision": "0a4061707e09a2966f209dda35f85564",
+    "url": "/deutsch/static/media/germany.0a406170.png"
+  },
+  {
+    "revision": "d11cc7b0b1c31324f8396de5e7ce5600",
+    "url": "/deutsch/index.html"
+  }
+];
